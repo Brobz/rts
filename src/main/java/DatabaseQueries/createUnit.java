eddba76dbea2b5c8759d8e44e12bcd7d68dd744d
Gibran Gonzalez <hector.gibran@hotmail.com>
2019-05-06 10:41:37 -0500

    Created query package, added createUnit class

---
+++ src/main/java/DatabaseQueries/createUnit.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DatabaseQueries;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Gibran Gonzalez
+ */
+public class createUnit {
+    
+    public ArrayList<createUnitQuery> arr;
+    
+    public static class createUnitQuery {
+        public static int id;
+        public static int edificioId;
+        public static int vida;
+        public static int costo;
+        
+    }
+}
